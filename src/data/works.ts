e689888a23cc4be457add39a8a8379ffa9dd3173
Sorami Hisamoto <s@89.io>
2023-02-17 15:53:53 +0900

    works

--- src/data/works.ts
+++ src/data/works.ts
@@ -2,6 +2,18 @@ import type { WorkItem } from "../types";
 
 export const works: WorkItem[] = [
   {
+    title: "World Ski Resorts - voronoi",
+    url: "https://observablehq.com/@sorami/world-ski-resorts-voronoi",
+    description: "Data from OpenStreetMap.",
+    image: "_assets/works/world-ski-resorts-voronoi.png",
+  },
+  {
+    title: "World Ski Resorts - nearest neighbors",
+    url: "https://observablehq.com/@sorami/world-ski-resorts-nearest-neighbors",
+    description: "",
+    image: "_assets/works/world-ski-resorts-nearest-neighbors.png",
+  },
+  {
     title: "Japanese from Hokkaido to Okinawa",
     url: "https://observablehq.com/@sorami/japanese-from-hokkaido-to-okinawa",
     description: "Population of Japanese cities plotted by their latitudes.",
@@ -10,13 +22,13 @@ export const works: WorkItem[] = [
   {
     title: "HOKKAIDLE",
     url: "https://sorami.dev/hokkaidle/",
-    description: "北海道の市町村を地図で当てるゲーム。Inspired by Wordle.",
+    description: "北海道の市町村を地図で当てるゲーム",
     image: "_assets/works/hokkaidle.png",
   },
   {
     title: "People Map of Japan",
     url: "https://sorami.dev/2021/people-map-japan/",
-    description: "「地名」を出身人物に置き換えた地図。",
+    description: "「地名」を出身人物に置き換えた地図",
     image: "_assets/works/people-map-japan.png",
   },
 ];
